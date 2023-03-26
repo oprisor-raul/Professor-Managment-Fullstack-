01cd29f78ed092853b81cba09efbb811b2b2f4d6
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2023-03-26 15:50:05 +0300

    1.0.4 Commit

--- src/main/java/com/oprisorraul/demo/DemoApplication.java
+++ src/main/java/com/oprisorraul/demo/DemoApplication.java
@@ -3,9 +3,7 @@ package com.oprisorraul.demo;
 import com.oprisorraul.demo.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,7 +32,9 @@ public class DemoApplication {
 			String email,
 			Integer age
 	) {}
-	public void addCustomer(NewCustomerRequest request) {
+
+	@PostMapping
+	public void addCustomer(@RequestBody NewCustomerRequest request) {
 		Customer customer = new Customer();
 		customer.setName(request.name());
 		customer.setEmail(request.email);
